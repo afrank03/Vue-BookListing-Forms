9d4d65654696f6ce869daa32e53b561f729359f7
Arthur Franks <Arthur.Franks@gocompare.com>
2019-01-09 14:18:44 +0000

    [REFACTOR] Updating book form to have bookData

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,17 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <div class="c-book-form__book--finished">
+      <label for="finishedReading">Finished Reading</label>
+      <input type="checkbox" id="finishedReading" v-model="bookData.finishedReading" />
+
+      <label for="borrowed">Borrowed</label>
+      <input type="radio" id="borrowed" v-model="bookData.ownership" value="borrowed" />
+
+      <label for="bought">bought</label>
+      <input type="radio" id="bought" v-model="bookData.ownership" value="bought" />
+    </div>
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
@@ -12,13 +22,17 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: [],
+      }
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
